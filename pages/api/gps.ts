cc14f77dfce489b40078721749821de129609e5a
bachillerraul-del <bachiller.raul@gmail.com>
2025-08-29 10:36:05 +0200

    migrar

--- pages/api/gps.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { getState } from "./_store";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { gps } = getState();
-  res.status(200).json({ ok: true, gps });
-}
